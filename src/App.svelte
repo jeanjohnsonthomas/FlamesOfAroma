3c960eaca7e042db37ca97e664dea6461eb6b4b3
Jean-Johnson <jeanjohnsonldrs@gmail.com>
2024-01-05 01:13:50 +0530

    old one

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <main>
-	<h1>{name}</h1>
-	<p>🛠️ Under Construction 🛠️</p>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
 <style>
